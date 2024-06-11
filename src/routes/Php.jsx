cd0d6bf8fcd4f625e51efbdf1f589c8d45bc3703
Diana Gpe. González Alfaro <82452616+DianaGA18@users.noreply.github.com>
2024-06-11 13:21:51 -0600

    Update Php.jsx

--- src/routes/Php.jsx
+++ src/routes/Php.jsx
@@ -7,8 +7,8 @@ const Php = () => {
         <h1>C</h1>
             <br/>
             <br/>
-            <h3>Se han elaborado varios proyectos con este lenguaje, utilzando Jupyter Notebook o Visual Studio Code.
-              Aqui se presentan algunos proyectos relacionados con Python.
+            <h3>Se elaboraron varios scripts relacionados con el lenguaje C, estos son básicos de implementar
+              es un acercamiento con este lenguaje.
             </h3>
         <a href="https://github.com/DianaGA18/Lenguaje-C" target="_blank" rel="noopener noreferrer">
           <button className='mt-14' style={{ backgroundColor: '#555555', color: 'white', padding: '10px 20px', border: 'none', borderRadius: '5px' }}>
